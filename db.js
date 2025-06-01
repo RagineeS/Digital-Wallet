c0eee8335a4bcb32316456def27da29492f8126d
Raginee <ragineesingh2005@gmail.com>
2025-06-01 13:05:05 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,1 @@
-// backend/config/db.js
-const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/digitalwallet');
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
-  }
-};
-module.exports = connectDB;
